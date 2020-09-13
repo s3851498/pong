2ca18082412724d8cd6c7b1ee4f5a3d6b5bba151
Mash J Prime <mashjprime@gmail.com>
2020-09-13 17:50:02 +0800

    Score System
    Added a scoring system and made modifications to the game so my kids can play happily

---
+++ Pong/Assets/GameManager.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+    {
+
+
+
+    // Player scores
+    public static int Player1Score = 0;
+    public static int Player2Score = 0;
+    public int maxScore = 10;
+
+    public GUISkin layout;
+
+    GameObject ball;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ball = GameObject.FindGameObjectWithTag("Ball"); 
+    }
+
+    void OnGUI()
+    {
+        GUI.skin = layout;
+        GUI.Label(new Rect(Screen.width / 2 - 150 - 12, 20, 100, 100), "" + Player1Score);
+        GUI.Label(new Rect(Screen.width / 2 + 150 + 12, 20, 100, 100), "" + Player2Score);
+
+        if (GUI.Button(new Rect(Screen.width / 2 - 60, 35, 120, 50), "Restart"))
+        {
+            Player1Score = 0;
+            Player2Score = 0;
+            ball.SendMessage("RestartGame", 0.5f, SendMessageOptions.RequireReceiver);
+        }
+
+        if (Player1Score == maxScore)
+        {
+            GUI.Label(new Rect(Screen.width / 2 - 150, 200, 2000, 1000), "Player 1 Wins");
+            ball.SendMessage("ResetToCenter", null, SendMessageOptions.DontRequireReceiver);
+            Invoke("ScoreReset", 2);
+        }
+        else if (Player2Score == maxScore)
+        {
+            GUI.Label(new Rect(Screen.width / 2 - 150, 200, 2000, 1000), "Player 2 Wins");
+            ball.SendMessage("ResetToCenter", null, SendMessageOptions.DontRequireReceiver);
+            Invoke("ScoreReset", 2);
+        }
+    }
+
+    // Add score based on collision with wall (Check tag)
+    public static void Score (string wall)
+    {
+        if (wall == "Left")
+        {
+            Player2Score++;
+        } 
+        else if (wall == "Right")
+        {
+            Player1Score++;
+        }
+    }
+
+    void ScoreReset()
+    {
+        Player1Score = 0;
+        Player2Score = 0;
+    }
+
+}
---
+++ Pong/Assets/Scripts/ScoreZone.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreZone : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.name == "Ball")
+        {
+            string wall = transform.name;
+            GameManager.Score(wall);
+            collision.gameObject.SendMessage("RestartGame", 1.0f, SendMessageOptions.RequireReceiver);
+        }
+    }
+}
